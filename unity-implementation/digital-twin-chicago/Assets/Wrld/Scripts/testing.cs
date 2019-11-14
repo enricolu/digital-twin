84ce435f6165433d4cad78a8a78dbe7257349304
andreology <37252145+andreology@users.noreply.github.com>
2019-11-13 16:57:36 -0800

    enlarging grid. adding click functionality to click in quadrant of grid

--- unity-implementation/digital-twin-chicago/Assets/Wrld/Scripts/testing.cs
+++ unity-implementation/digital-twin-chicago/Assets/Wrld/Scripts/testing.cs
@@ -10,7 +10,7 @@ public class testing : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        grid = new Grid(20, 10, 20f, new Vector3(20, 0, 0));
+        grid = new Grid(20, 10, 20f, Vector3.zero);
     }
 
     // Update is called once per frame
